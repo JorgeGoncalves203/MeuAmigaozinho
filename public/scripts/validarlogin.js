3d9c5d8aa44fd987c7aa22b43a535a323df6fd5a
Jorge Gonçalves <mg4426231@gmail.com>
2023-10-28 04:15:32 -0300

    criando requisitos para não deixar os campos vazios

--- public/scripts/validarlogin.js
+++ public/scripts/validarlogin.js
@@ -1,10 +1,11 @@
-function validateFields() {
-    const emailValid = isEmailValid();
-    document.getElementById('recover-password-button').disabled = !emailValid;
+function OnChangeEmail() {
+    toggleEmailErros();
+    toggleButtonsDisable();
+}
 
-
-    const passwordValid = isPasswordValid();
-    document.getElementById('login-button').disabled = !emailValid || !passwordValid;
+function OnChangePassword() {
+    toggleButtonsDisable();
+    togglePasswordErros();
 }
 
 function isEmailValid() {
@@ -13,6 +14,42 @@ function isEmailValid() {
         return false;
     }
     return validateEmail(email);
+}
+
+
+function toggleEmailErros() {
+    const email = document.getElementById('email').value;
+    if (!email) {
+        document.getElementById('email-required-error').style.display = "block";
+    } else {
+        document.getElementById('email-required-error').style.display = "none";
+    }
+
+    if (validateEmail(email)) {
+        document.getElementById('email-invalid-error').style.display = "none";
+    } else {
+        document.getElementById('email-invalid-error').style.display = "block";
+    }
+}
+    
+
+
+function togglePasswordErros() {
+    const password = document.getElementById('password').value;
+    if (!password) {
+        document.getElementById('password-required-error').style.display = "block";
+    } else {
+        document.getElementById('password-required-error').style.display = "none";
+    }
+}
+
+function toggleButtonsDisable() {
+    const emailValid = isEmailValid();
+    document.getElementById('recover-password-button').disabled = !emailValid;
+
+
+    const passwordValid = isPasswordValid();
+    document.getElementById('login-button').disabled = !emailValid || !passwordValid;
 }
 
 function isPasswordValid() {
