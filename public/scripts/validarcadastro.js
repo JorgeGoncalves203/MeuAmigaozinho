e58c1edfc21cef836f491add9d6705f59d6c8b9b
JorgeGoncalves203 <mg4426231@gmail.com>
2023-11-02 02:24:37 -0300

    ajuste de login e cadastro

--- public/scripts/validarcadastro.js
+++ public/scripts/validarcadastro.js
@@ -1,15 +1,76 @@
 function OnChangeEmail() {
-    toggleEmailErros();
-    toggleButtonsDisable();
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? "none" : "block";
+
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
+
+    toggleRegisterButtonDisable();
+}
+
+function OnChangePassword() {
+    const password = form.password().value;
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+
+    form.passwordMinLengthError().style.display = password.length >= 6 ? "none" : "block";
+
+    validatePasswordsMatch();
+    toggleRegisterButtonDisable();
+
+}
+
+function OnChangeConfirmPassword() {
+    validatePasswordsMatch();
+    toggleRegisterButtonDisable();
+}
+
+function validatePasswordsMatch() {
+    const password = form.password().value;
+    const confirmPassword = form.confirmPassword().value;
+
+    form.confirmPasswordDoenstMatchError().style.display = 
+    password == confirmPassword ? "none" : "block";
+
+}
+
+function toggleRegisterButtonDisable() {
+    form.registerButton().disabled = !isFormValid();
+}
+
+
+function isFormValid() {
+    const email = form.email().value;
+    if (!email || !validateEmail(email)) {
+        return false;
+    }
+
+    const password = form.password().value;
+    if (!password || password.length < 6) {
+        return false;
+    }
+    return true;
+
 }
 
 
 
+
+
+
+
+
+
+
+
+
 const form = {
+    
+    confirmPassword: () => document.getElementById('confirmPassword'),
+    confirmPasswordDoenstMatchError: () => document.getElementById('password-doenst-match-error'),
     email: () => document.getElementById('email'),
     emailInvalidError: () => document.getElementById('email-required-error'),
     emailRequiredError: () => document.getElementById('email-invalid-error'),
     password: () => document.getElementById('password'),
+    passwordMinLengthError: () => document.getElementById('password-min-length-error'),
     passwordRequiredError: () => document.getElementById('password-required-error'),
-    recoverPassword: () => document.getElementById('recover-password-button')
+    registerButton: () => document.getElementById('register-button')
 }
