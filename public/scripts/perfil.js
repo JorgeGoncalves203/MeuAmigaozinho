a50850e4278fc8199ca0f351b3e5b03f88bf79e9
Andriw Daniel <andriwdaniel04@gmail.com>
2023-11-03 04:14:34 -0300

    Conseguir alterar nome e telefone no perfil

--- public/scripts/perfil.js
+++ public/scripts/perfil.js
@@ -1,5 +1,283 @@
-import { signOut } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js"
+import { signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js"
+import { collection, getDocs, doc, query, where, onSnapshot, updateDoc } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
 import { auth } from "./firebaseConfig.js"
+import { db } from "./firebaseConfig.js";
+
+const colRef = collection(db, 'usuarios')
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      usuarioLogado(user)
+    } 
+  });
+
+function usuarioLogado(user) {
+    const q = query(colRef, where("email", "==", user.email))
+    
+    onSnapshot(q, snapshot => {
+        const usuario = snapshot.docs.map(doc => doc.data()); 
+        informacoesUsuario(usuario)
+        })
+} 
+
+function informacoesUsuario(usuario) {
+    
+    usuario.forEach(usuario => {
+        const perfilEditarForm = document.querySelector('.perfil-form')
+        
+        perfilEditarForm.name.value = usuario.nome
+        perfilEditarForm.email.value = usuario.email
+        perfilEditarForm.tel.value = usuario.telefone
+        perfilEditarForm.senha.value = "********"
+    })
+}
+
+//Editar Nome
+
+const editarNomeButton = document.getElementById('editarNome')
+editarNomeButton.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const div = document.createElement('div')
+    div.classList.add('fundoModal')
+    
+    const div1 = document.createElement('div')
+    div1.classList.add('modalEditarNome')
+    
+    const h2 = document.createElement('h3')
+    h2.textContent = 'Altere seu Nome:'
+
+    const buttonFechar = document.createElement('button')
+    buttonFechar.classList.add('botaoFechar')
+
+    const img = document.createElement('img')
+    img.src = `../img/fechar.png`
+
+    const formNome = document.createElement('form')
+    formNome.classList.add('formNome')
+
+    const input = document.createElement('input')
+    input.type = 'text'
+    input.name = 'name'
+    input.id = 'namen'
+
+    const buttonSalvar = document.createElement('button')
+    buttonSalvar.classList.add('botaoSalvar')
+    buttonSalvar.textContent = 'Salvar alterações'
+    
+    div.appendChild(div1)
+    div1.appendChild(h2)
+    div1.appendChild(buttonFechar)
+    buttonFechar.appendChild(img)
+    div1.appendChild(formNome)
+    formNome.appendChild(input)
+    formNome.appendChild(buttonSalvar)
+    document.body.appendChild(div);
+    
+    
+    const fecharButton = document.querySelector('.botaoFechar')
+    fecharButton.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const fundoModal = document.getElementsByClassName('fundoModal');
+    if (fundoModal.length) {
+        fundoModal[0].remove();
+    }
+    })
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+        usuarioLogado(user)
+        } 
+    });
+
+    function usuarioLogado(user) {
+        const q = query(colRef, where("email", "==", user.email))
+        
+        onSnapshot(q, snapshot => {
+            const usuario = snapshot.docs.map(doc => doc.data()); 
+            informacoesUsuario(usuario)
+            })
+    } 
+
+    function informacoesUsuario(usuario) {
+        
+        usuario.forEach(usuario => {
+            const formNome = document.querySelector('.formNome')
+            
+            formNome.name.value = usuario.nome
+        })
+    }
+
+    const colRef = collection(db, 'usuarios')
+
+    const salvarButon = document.querySelector('.botaoSalvar')
+    salvarButon.addEventListener('click', (e) => {
+    e.preventDefault()
+
+        onAuthStateChanged(auth, (user) => {
+        if (user) {
+        usuarioLog(user)
+        } 
+    });
+
+    function usuarioLog(user) {
+        const q = query(colRef, where("email", "==", user.email))
+        
+        onSnapshot(q, snapshot => {
+            const usuario = snapshot.docs.map(doc => doc.id); 
+            fazerUpdate(usuario)
+            })
+    }
+    
+    })
+
+    function fazerUpdate(usuarioId) {
+
+        usuarioId.forEach(usuarioId => {
+        const docRef = doc(db, 'usuarios', usuarioId)
+
+        const formNome = document.querySelector('.formNome')
+
+        updateDoc(docRef, {
+            nome: formNome.name.value
+        })
+        .then(() => {
+            const fundoModal = document.getElementsByClassName('fundoModal');
+    if (fundoModal.length) {
+        fundoModal[0].remove();
+    }
+    })
+    })
+    }
+
+
+
+})
+
+//Editar Telefone
+
+const editarTelefoneButton = document.getElementById('editarTelefone')
+editarTelefoneButton.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const div = document.createElement('div')
+    div.classList.add('fundoModal')
+    
+    const div1 = document.createElement('div')
+    div1.classList.add('modalEditarNome')
+    
+    const h2 = document.createElement('h3')
+    h2.textContent = 'Altere seu Telefone:'
+
+    const buttonFechar = document.createElement('button')
+    buttonFechar.classList.add('botaoFechar')
+
+    const img = document.createElement('img')
+    img.src = `../img/fechar.png`
+
+    const formTel = document.createElement('form')
+    formTel.classList.add('formTel')
+
+    const input = document.createElement('input')
+    input.type = 'tel'
+    input.name = 'tel'
+    input.id = 'phonen'
+
+    const buttonSalvar = document.createElement('button')
+    buttonSalvar.classList.add('botaoSalvar')
+    buttonSalvar.textContent = 'Salvar alterações'
+    
+    div.appendChild(div1)
+    div1.appendChild(h2)
+    div1.appendChild(buttonFechar)
+    buttonFechar.appendChild(img)
+    div1.appendChild(formTel)
+    formTel.appendChild(input)
+    formTel.appendChild(buttonSalvar)
+    document.body.appendChild(div);
+    
+    
+    const fecharButton = document.querySelector('.botaoFechar')
+    fecharButton.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const fundoModal = document.getElementsByClassName('fundoModal');
+    if (fundoModal.length) {
+        fundoModal[0].remove();
+    }
+    })
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+        usuarioLogado(user)
+        } 
+    });
+
+    function usuarioLogado(user) {
+        const q = query(colRef, where("email", "==", user.email))
+        
+        onSnapshot(q, snapshot => {
+            const usuario = snapshot.docs.map(doc => doc.data()); 
+            informacoesUsuario(usuario)
+            })
+    } 
+
+    function informacoesUsuario(usuario) {
+        
+        usuario.forEach(usuario => {
+            const formTel = document.querySelector('.formTel')
+            
+            formTel.tel.value = usuario.telefone
+        })
+    }
+
+    const colRef = collection(db, 'usuarios')
+
+    const salvarButon = document.querySelector('.botaoSalvar')
+    salvarButon.addEventListener('click', (e) => {
+    e.preventDefault()
+
+        onAuthStateChanged(auth, (user) => {
+        if (user) {
+        usuarioLog(user)
+        } 
+    });
+
+    function usuarioLog(user) {
+        const q = query(colRef, where("email", "==", user.email))
+        
+        onSnapshot(q, snapshot => {
+            const usuario = snapshot.docs.map(doc => doc.id); 
+            fazerUpdate(usuario)
+            })
+    }
+    
+    })
+
+    function fazerUpdate(usuarioId) {
+
+        usuarioId.forEach(usuarioId => {
+        const docRef = doc(db, 'usuarios', usuarioId)
+
+        const formNome = document.querySelector('.formTel')
+
+        updateDoc(docRef, {
+            telefone: formNome.tel.value
+        })
+        .then(() => {
+            const fundoModal = document.getElementsByClassName('fundoModal');
+    if (fundoModal.length) {
+        fundoModal[0].remove();
+    }
+    })
+    })
+    }
+
+
+
+})
+
 
 const logoutButton = document.querySelector('.sair')
 logoutButton.addEventListener('click', () => {
